48595a75396862c5b1ac2d428fd1c3a75a469659
Ahmed Elgamal <elgamala@amazon.de>
2021-03-11 22:13:57 +0100

    Infrastructure versioning

--- bin/cdk-hackathon.ts
+++ bin/cdk-hackathon.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkHackathonStack } from '../lib/stacks/cdk-hackathon-stack';
+import * as git from 'git-last-commit'
+import {CdkHackathonStack} from '../lib/stacks/cdk-hackathon-stack';
 
 const app = new cdk.App();
 new CdkHackathonStack(app, 'CdkHackathonStack');
+
+git.getLastCommit((err, commit) => {
+    if (commit && !err) {
+        cdk.Tags.of(app).add('CDK/Git/Repo/name', 'https://github.com/aserour/cdk-hackathon.git')
+        cdk.Tags.of(app).add('CDK/Git/CommitHash', commit.shortHash)
+        cdk.Tags.of(app).add('CDK/Git/CommitMessage', commit.sanitizedSubject)
+        cdk.Tags.of(app).add('CDK/Git/Author/Name', commit.author.name)
+        cdk.Tags.of(app).add('CDK/Git/Author/Email', commit.author.email)
+        cdk.Tags.of(app).add('CDK/Git/Branch', commit.branch)
+        cdk.Tags.of(app).add('CDK/Git/Timestamp', commit.committedOn)
+    }
+})
